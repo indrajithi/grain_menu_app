22a9d33e0fb7395280c03f75abe0bde45322fa8f
Indrajith Indraprastham <mail@indrajith.me>
2024-07-23 19:53:39 +0530

    Graphql queries types and queries related to section

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,5 +18,25 @@ module Types
     def menu(id:)
       Menu.find(id)
     end
+
+    # Add Section Queries
+    field :sections, [Types::SectionType], null: false do
+      description 'Retrieve all sections'
+    end
+
+    def sections
+      Section.all
+    end
+
+    field :section, Types::SectionType, null: false do
+      description 'Retrieve a single section'
+      argument :id, ID, required: true
+    end
+
+    def section(id:)
+      Section.find(id)
+    end
+
+
   end
 end
---
+++ app/graphql/types/section_type.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Types
+  class SectionType < Types::BaseObject
+    field :id, ID, null: false
+    field :identifier, String
+    field :label, String
+    field :description, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :items, [Types::ItemType], null: true
+
+    def items
+      object.items
+    end
+  end
+end
