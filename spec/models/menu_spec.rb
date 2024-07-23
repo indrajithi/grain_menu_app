39e5a96203bbaac6019b54b70dc573559da8feee
Indrajith Indraprastham <mail@indrajith.me>
2024-07-23 17:26:02 +0530

    add menu_spec and .rubocop

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
@@ -17,28 +15,6 @@ RSpec.describe Menu, type: :model do
     it 'is not valid without a state' do
       menu = build(:menu, state: nil)
       expect(menu).not_to be_valid
-    end
-  end
-
-  describe 'database columns' do
-    it 'has a label column of type string' do
-      column = Menu.columns_hash['label']
-      expect(column.type).to eq :string
-    end
-
-    it 'has a state column of type string' do
-      column = Menu.columns_hash['state']
-      expect(column.type).to eq :string
-    end
-
-    it 'has a start_date column of type date' do
-      column = Menu.columns_hash['start_date']
-      expect(column.type).to eq :date
-    end
-
-    it 'has an end_date column of type date' do
-      column = Menu.columns_hash['end_date']
-      expect(column.type).to eq :date
     end
   end
 end
